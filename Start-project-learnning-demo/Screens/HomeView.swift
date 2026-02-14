7a1107c8ab9eafdb150756d969154ec4bcef050d
Ros Dul <dul@Duls-MacBook-Pro.local>
2026-02-14 17:57:46 +0700

    Add home design and image animate

--- Start-project-learnning-demo/Screens/HomeView.swift
+++ Start-project-learnning-demo/Screens/HomeView.swift
@@ -11,14 +11,41 @@ struct HomeView: View {
     @AppStorage("onboarding") var isHomeActive: Bool = false
     var body: some View {
         VStack(spacing: 20){
-            Text("home".uppercased())
-                .font(.largeTitle)
-            Button {
-                isHomeActive = true
-            } label: {
-                Text("Reset")
-                    .font(.headline)
+            //MARK: -HEARDER
+            Spacer()
+            
+            ZStack{
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.4)
+                
+                Image("imageDul")
+                    .resizable()
+                    .scaledToFit()
             }
+            
+            //MARK: -CENTER
+            Text(
+                "I has been learned swift UIKIT and have a one project but now swiftui is populer than swift UIKIT so I will try to learn swiftUI. I will try to make a simple app to show you how to use it."
+            )
+            .padding()
+            .multilineTextAlignment(TextAlignment.center)
+            .font(.system(.title3))
+            .foregroundColor(.gray)
+            
+            
+            //MARK: -FOOTER
+                Button {
+                    isHomeActive = true
+                } label: {
+                    Image(systemName: "arrow.trianglehead.2.clockwise.rotate.90.circle.fill")
+                        .imageScale(.large)
+                    
+                    Text("Restart")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
 
         }
     }
